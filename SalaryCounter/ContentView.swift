1fd243b06869a8bb1c64f866b856e9ce2962f8fd
Birk Eidsvik <bbe@dips.no>
2023-10-05 13:43:36 +0200

    Initial Commit

---
+++ SalaryCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SalaryCounter
+//
+//  Created by Birk Eidsvik on 05/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SalaryCounter/SalaryCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalaryCounterApp.swift
+//  SalaryCounter
+//
+//  Created by Birk Eidsvik on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SalaryCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
